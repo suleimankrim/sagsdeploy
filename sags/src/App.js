6f02d717772fbc83088dbcc5878d39b6aaff96f2
SuleimanKarimEddin <sulimankareem90@gmail.com>
2023-06-19 23:52:27 +0300

    m

--- sags/src/App.js
+++ sags/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Table } from './Components/Table';
-
  const App = () => {
   return (
     <>
--- sags/src/Components/Table.js
+++ sags/src/Components/Table.js
@@ -2,6 +2,7 @@ import React from 'react'
 import DataTable from 'react-data-table-component';
 import columns from './Columns';
 import data from './MockData';
+import Checkbox from '@material-ui/core/Checkbox';
 export const Table = () => {
   return (
     <DataTable
