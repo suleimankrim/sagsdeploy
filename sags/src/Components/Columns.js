59eafdfe88338708fac7de96e7a3edd1c658d275
SuleimanKarimEddin <sulimankareem90@gmail.com>
2023-06-20 01:26:28 +0300

    k

--- sags/src/Components/Columns.js
+++ sags/src/Components/Columns.js
@@ -1,10 +1,20 @@
+import Style from "./Cell.module.css";
 const columns = [
     {
         name: 'Title',
         selector: row => row.title,
+        sortable: true,
+        cell : row =>(<>
+      <div className={Style.otercell}>
+        <input type="text" className= {Style.cell} placeholder="kkkk"  />
+        </div>
+        </>)
     },
     {
         name: 'Year',
         selector: row => row.year,
+        style:{
+            backgroundColor:'red'
+        }
     }]
 export default columns;
