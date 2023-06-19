63f9f068451eec5d02c838a228d1ca383dec15fa
SuleimanKarimEddin <sulimankareem90@gmail.com>
2023-06-20 00:08:40 +0300

    hi

--- sags/src/Components/Table.js
+++ sags/src/Components/Table.js
@@ -3,7 +3,10 @@ import DataTable from 'react-data-table-component';
 import columns from './Columns';
 import data from './MockData';
 import {checkbox} from"@mui/material"
+import {ArrowDownward} from '@mui/material/';
 export const Table = () => {
+    const sortIcon = <ArrowDownward />;
+const selectProps = { indeterminate: isIndeterminate => isIndeterminate };
   return (
     <DataTable
             columns={columns}
