87b8f2cd3f492b2e3976511448117efa19ea348c
Hamza-Shafee-Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-06-19 23:58:51 +0300

    hhh

--- sags/src/Components/Table.js
+++ sags/src/Components/Table.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DataTable from 'react-data-table-component';
 import columns from './Columns';
 import data from './MockData';
-import Checkbox from '@material-ui/core/Checkbox';
+import {checkbox} from"@mui/material"
 export const Table = () => {
   return (
     <DataTable
