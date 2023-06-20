a0ec0d0ac2e558aff725d55bf44dc575c8778553
Hamza-Shafee-Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-06-20 23:16:13 +0300

    FINISH header

--- sags/src/Components/header/right section/RightSection.js
+++ sags/src/Components/header/right section/RightSection.js
@@ -3,10 +3,11 @@ import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 import Style from './RightSection.module.css';
 export const RightSection = () => {
     const [name,setname]=useState("User name");
+    const [image,setimage]=useState( <AccountCircleRoundedIcon sx={{fontSize:30}} ></AccountCircleRoundedIcon>);
   return (
     <div className={Style.Container}>
         <div className={Style.Userphotocontainer}>
-        <AccountCircleRoundedIcon sx={{fontSize:30}} ></AccountCircleRoundedIcon>
+        {image}
         </div>
         <div className={Style.Username}>
         {name}
