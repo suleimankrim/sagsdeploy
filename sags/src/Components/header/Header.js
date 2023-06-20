23568010e6aa3a0bd9d1b84a220919396253dbb7
Hamza-Shafee-Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-06-20 11:43:02 +0300

    header

---
+++ sags/src/Components/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Style from './Header.module.css'
+export const Header = () => {
+  return (
+    <div className='Style.header'> <button>
+        pressHERE
+        </button>
+        <div>
+            middlesection
+        </div>
+        <div>
+            righttserction
+        </div>
+        </div>
+  )
+}
