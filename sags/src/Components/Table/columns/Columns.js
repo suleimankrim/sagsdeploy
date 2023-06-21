e2f73ec52c1db889ac160920e82414b6219811e3
SuleimanKarimEddin <sulimankareem90@gmail.com>
2023-06-21 11:49:15 +0300

    mnka

--- sags/src/Components/Table/columns/Columns.js
+++ sags/src/Components/Table/columns/Columns.js
@@ -5,19 +5,23 @@ import { HeadForm } from "../head-cell/HeadForm";
 import { TitleForm } from "../title-cell/TitleForm";
 const title = Object.keys(data[0]);
 const columns = alphabet.map((al, index) => {
-  return {
-    name: <HeadForm name={al} key={index} />,
+    return {
+      name: <HeadForm name={al} key={index} />,
 
-    cell: (row, idx) => (
-      <>
-        {idx === 0 ? (
-          <TitleForm name={index === 0? 1:title[index-1]} key={index + 10000} />
-        ) : (
-          <CellForm row={row} index={index} key={row.id} />
-        )}
-      </>
-    ),
-  };
+      cell: (row, idx) => (
+        <>
+          {idx === 0 ? (
+            <TitleForm
+              name={index === 0 ? 1 : title[index - 1]}
+              key={index + 10000}
+              index = {index}
+            />
+          ) : (
+            <CellForm row={row} index={index} key={row.id} />
+          )}
+        </>
+      )
+    };
 });
 
 export default columns;
