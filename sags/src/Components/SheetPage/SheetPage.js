a6430541adbe2cd871a833d3e529676a4f88258b
Hamza-Shafee-Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-06-22 13:29:07 +0300

    pages

---
+++ sags/src/Components/SheetPage/SheetPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../header/Header'
+import SearchBoxContainer from '../SearchBoxContainer/SearchBoxContainer'
+import { Table } from '../Table/Table'
+
+export const SheetPage = () => {
+  return (
+    <>
+      <Header />
+      <SearchBoxContainer />
+                <Table/>
+
+    </>
+  )
+}
+export default SheetPage;
