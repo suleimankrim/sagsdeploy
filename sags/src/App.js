cb9adfc70d6e7ad881165e8c40fc874412485b3c
Hamza-Shafee-Aldaghstany <118183676+Hamza-Shafee-Aldaghstany@users.noreply.github.com>
2023-06-22 13:29:22 +0300

    pagesfinish

--- sags/src/App.js
+++ sags/src/App.js
@@ -3,20 +3,16 @@ import { Header } from "./Components/header/Header";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { SearchBoxContainer } from "./Components/SearchBoxContainer/SearchBoxContainer";
+import { SheetPage } from "./Components/SheetPage/SheetPage";
 const App = () => {
   return (
     <>
-      <Header />
       <BrowserRouter>
         <Routes>
           <Route
             path="/"
             element={
-              <>
-                <SearchBoxContainer />
-                <Table />
-             
-              </>
+              <SheetPage/>
             }
           />
           {/* <Route index element={<Home />} /> */}
